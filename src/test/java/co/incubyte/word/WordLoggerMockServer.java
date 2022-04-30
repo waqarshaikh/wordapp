c1ffcfb7a546cd7bb3dbbf797684c6cfba518a33
Waqar <waqar@incubyte.co>
2022-04-30 08:56:16 +0530

    ENH: Added WireMockServer

---
+++ src/test/java/co/incubyte/word/WordLoggerMockServer.java
@@ -0,0 +1,44 @@
+package co.incubyte.word;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+
+public class WordLoggerMockServer {
+
+  private static WireMockServer wireMockServer;
+
+  public static void start() {
+    if (wireMockServer == null || !wireMockServer.isRunning()) {
+      startWithoutStubs();
+    }
+    addGetWordsEndpoint();
+  }
+
+  public static void addGetWordsEndpoint() {
+    wireMockServer.stubFor(
+        WireMock.post(WireMock.urlMatching("/retrieve"))
+            .willReturn(WireMock.okJson(getWordsLoggedData())));
+  }
+
+  private static String getWordsLoggedData() {
+    return "{\n"
+        + "    \"message\":  \"Logged Word Retrieval successfully\"\n"
+        + "}";
+  }
+
+  public static void startWithoutStubs() {
+    wireMockServer = new WireMockServer(
+        options().port(8000).notifier(new ConsoleNotifier(true))
+    );
+    wireMockServer.start();
+  }
+
+  public static void stop() {
+    if (wireMockServer != null && wireMockServer.isRunning()) {
+      wireMockServer.stop();
+    }
+  }
+}
