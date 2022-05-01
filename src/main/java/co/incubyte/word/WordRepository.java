b08783fd9ccc62c3592944cf0caa22ac3400bf86
Waqar <waqar@incubyte.co>
2022-05-01 08:49:32 +0530

    ENH: Added SQL Database

--- src/main/java/co/incubyte/word/WordRepository.java
+++ src/main/java/co/incubyte/word/WordRepository.java
@@ -1,15 +1,13 @@
 package co.incubyte.word;
 
+import io.micronaut.data.annotation.Query;
+import io.micronaut.data.annotation.Repository;
+import io.micronaut.data.repository.CrudRepository;
 import java.util.List;
-import javax.inject.Singleton;
 
-@Singleton
-public class WordRepository {
+@Repository
+public interface WordRepository extends CrudRepository<Word, Long> {
 
-  public List<Word> findAll() {
-    return List.of(
-        new Word("Hello"),
-        new Word("World")
-    );
-  }
+  @Query("select w from Word w")
+  List<Word> getAllWords();
 }
